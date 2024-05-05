75a552680842efcb77551d6979d2b4d3c75c7192
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-04 21:41:07 -0400

    correcion de la convercion de valores

--- busqueda/js/app.js
+++ busqueda/js/app.js
@@ -3,17 +3,42 @@ async function obtenerTasaDeCambio() {
     const datos = await respuesta.json();
     return datos.rates.USD;
 }
+let isPrecioEnUSD = false; // Declarar isPrecioEnUSD como una variable global
 
-
+async function cambiar() {
+    const boton_clp_usd = document.getElementById('boton-clp-usd');
+    if (boton_clp_usd.innerHTML == 'CLP a USD') {
+        boton_clp_usd.innerHTML = 'USD a CLP';
+        isPrecioEnUSD = true;
+    } else {
+        boton_clp_usd.innerHTML = 'CLP a USD';
+        isPrecioEnUSD = false;
+    }
+    await actualizarPrecio();
+}
 
 async function actualizarPrecio() {
-    const elementosPrecio = document.querySelectorAll('#precio');
+    const elementosPrecio = document.querySelectorAll('#precio'); // Cambiado de '.precio' a '#precio'
     const tasaDeCambio = await obtenerTasaDeCambio();
 
-    elementosPrecio.forEach(async (elementoPrecio) => {
-        const precioEnCLP = Number(elementoPrecio.textContent);
-        const precioEnUSD = precioEnCLP * tasaDeCambio;
-        elementoPrecio.textContent = precioEnUSD.toFixed(2);
+    elementosPrecio.forEach((elementoPrecio) => {
+        let precioActual = Number(elementoPrecio.textContent);
+        if (isPrecioEnUSD) {
+            precioActual = precioActual * tasaDeCambio;
+        } else {
+            precioActual = precioActual / tasaDeCambio;
+        }
+        elementoPrecio.textContent = precioActual.toFixed(2);
     });
 }
+// async function actualizarPrecio() {
+//     const elementosPrecio = document.querySelectorAll('#precio');
+//     const tasaDeCambio = await obtenerTasaDeCambio();
+
+//     elementosPrecio.forEach(async (elementoPrecio) => {
+//         const precioEnCLP = Number(elementoPrecio.textContent);
+//         const precioEnUSD = precioEnCLP * tasaDeCambio;
+//         elementoPrecio.textContent = precioEnUSD.toFixed(2);
+//     });
+// }
 
