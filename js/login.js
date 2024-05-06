18907bae046c745b98373c4b0c0b74ffb2dc3832
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-06 14:22:17 -0400

    hola

---
+++ js/login.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('.btnLimpiar').click(function () {
+        $('.txtEmail, .txtClave').val('');
+    });
+    $('.btnLog').click(function () {
+        if ($('.txtEmail').val() == '') {
+            alert('No especificó el correo');
+        } else if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test($('.txtEmail').val())) {
+            alert('El formato del correo no es válido');
+        } else if ($('.txtClave').val() == "") {
+            alert('No especificó la clave');
+        } else if (!/^[a-zA-Z0-9]+$/.test($('.txtClave').val())) {
+            alert('La clave solo puede contener letras y números');
+        } else {
+            alert('Los datos se enviaron correctamente');
+        }
+    });
+});
