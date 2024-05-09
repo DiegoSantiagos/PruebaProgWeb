8f1c9826aae24d29daf3afe29e4c167ecccdbfcf
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-09 11:12:25 -0400

    topnav

---
+++ js/carrito.js
@@ -0,0 +1,13 @@
+// function carrito(x) {
+//     var i;
+//     x = document.getElementsByClassName("filterDiv");
+//     if (c == "all") c = "";
+//     for (i = 0; i < x.length; i++) {
+//         w3RemoveClass(x[i], "show");
+//         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//     }
+// }
+
+function carrito(x) {
+    var p = document.getElementsByClassName("filterDiv");
+}
