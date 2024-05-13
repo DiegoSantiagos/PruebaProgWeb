845bdf9c857117b011dd44067c704fe3ffe9db65
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-12 22:41:56 -0400

    carrito

--- js/carrito.js
+++ js/carrito.js
@@ -8,6 +8,27 @@
 //     }
 // }
 
-function carrito(x) {
-    var p = document.getElementsByClassName("filterDiv");
+// function carrito(x) {
+//     var p = document.getElementsByClassName("filterDiv");
+// }
+
+function carrito(id) {
+    // Añadir el id del producto al carrito en el localStorage
+    var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    carrito.push(id);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+window.onload = function () {
+    // Obtener los ids de los productos en el carrito del localStorage
+    var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    // Hacer visible cada producto en el carrito
+    for (var i = 0; i < carrito.length; i++) {
+        var id = carrito[i];
+        var elementoCarrito = document.getElementById('carrito' + id);
+        if (elementoCarrito) {
+            elementoCarrito.style.display = 'block';
+        }
+    }
 }
