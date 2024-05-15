c970c5e428b56aa03f8f52b203fa3b3ba8951d48
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-14 23:36:37 -0400

    carrito

--- carrito/js/cont.js
+++ carrito/js/cont.js
@@ -1,30 +1,14 @@
-
-function sumarCantidad() {
-    let cantidadInput = document.getElementById('cantidad2');
-    let cantidad = parseInt(cantidadInput.value);
-    cantidadInput.value = cantidad + 1;
-}
-
-function restarCantidad() {
-    let cantidadInput = document.getElementById('cantidad2');
-    let cantidad = parseInt(cantidadInput.value);
-    if (cantidad > 1) {
-        cantidadInput.value = cantidad - 1;
-    }
-}
-
-
 function sumarCanti(x) {
-    let cantidadInput = document.getElementById(x);
+    let cantidadInput = document.getElementById('cantidad'.padEnd(9, x));
     let cantidad = parseInt(cantidadInput.value);
-    cantidadInput.value = cantidad + 1;
+    document.getElementById('cantidad'.padEnd(9, x)).value = cantidad + 1;
 }
 
 function restarCanti(x) {
-    let cantidadInput = document.getElementById(x);
+    let cantidadInput = document.getElementById('cantidad'.padEnd(9, x));
     let cantidad = parseInt(cantidadInput.value);
     if (cantidad > 1) {
-        cantidadInput.value = cantidad - 1;
+        document.getElementById('cantidad'.padEnd(9, x)).value = cantidad - 1;
     }
 }
 
