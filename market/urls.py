9cd8ee1df01aa661f13fc6072d6df9b846de0779
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-06-17 02:40:36 -0400

    django listar y bd

--- market/urls.py
+++ market/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path(listarProductos, views.listarProductos, name='listarProductos'),
 ]
--- market/views.py
+++ market/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'market/index.html', context)
+
+def listarProductos(request):
+    context = {}
+    return render(request, 'market/listarProductos.html', context)
